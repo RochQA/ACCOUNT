6782986d1042a2cc9981809ca7652ff030e99e8b
shinigamirei <gadagack101@gmail.com>
2019-03-21 12:24:48 +0000

    Added tests for accountService class

--- AccountAPI/src/main/java/com/example/demo/service/AccountServiceImp.java
+++ AccountAPI/src/main/java/com/example/demo/service/AccountServiceImp.java
@@ -17,6 +17,7 @@ public class AccountServiceImp implements AccountService {
 	public AccountServiceImp(AccountRepository repo) {
 		this.repo = repo;
 	}
+	
 	@Override
 	public Account getAccount(Long id) {
 		List<Account> allAccounts = getAllAccounts();
