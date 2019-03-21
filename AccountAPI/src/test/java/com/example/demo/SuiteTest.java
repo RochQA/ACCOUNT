325326e3e2fcaca8defaefbc690214b91c5d33b0
shinigamirei <gadagack101@gmail.com>
2019-03-21 14:52:31 +0000

    added service tessts to the test suite

--- AccountAPI/src/test/java/com/example/demo/SuiteTest.java
+++ AccountAPI/src/test/java/com/example/demo/SuiteTest.java
@@ -6,7 +6,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 
-@SuiteClasses( { AccountEntityTests.class, AccountControllerTests.class, AccountApplicationTests.class, LoginEntityTests.class } )
+@SuiteClasses( { AccountEntityTests.class, AccountControllerTests.class, AccountServiceTests.class , AccountApplicationTests.class, LoginEntityTests.class } )
 
 public class SuiteTest {
 
